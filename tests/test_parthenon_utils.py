9da351b2d71cb2e611e7d0d13461622c41b15636
Miguel Couy @ Echoenergia <miguel.couy@outlook.com>
2024-04-26 18:40:17 -0300

    Update test_parthenon_utils.py
    Rework on base_session fixture and test_controller_basic_init

--- tests/test_parthenon_utils.py
+++ tests/test_parthenon_utils.py
@@ -11,20 +11,30 @@ import pytest
 
 import os
 import datetime as dt
+from pathlib import Path
 from cryptography.fernet import Fernet
+import shutil
+from time import sleep
 
 from source.parthenon_utils import Controller
 
 @pytest.fixture(scope='session')
-def base_session():
-    return {
+def base_session(tmp_path_factory):
+    base_path = tmp_path_factory.mktemp("data") 
+    secret_message = 'Corvus oculum corvi non eruit'
+    test_directory = base_path / "test_files"
+    test_directory.mkdir(exist_ok=True)
+    
+    yield {
         'code_filename': os.path.basename(__file__),
-        'code_directory': os.path.dirname(os.path.abspath(__file__)),
+        'code_directory': test_directory,
         'code_runtime': dt.datetime.now(),
         'code_fernet_key': Fernet.generate_key(),
-        'ctrl_create_base_paths': True
+        'ctrl_create_base_paths': True,
+        'secret_message': secret_message
     }
-
+    
+    shutil.rmtree(test_directory)
 
 def test_controller_basic_init(base_session):
     ctrl = Controller(
@@ -35,8 +45,11 @@ def test_controller_basic_init(base_session):
         ctrl_create_base_paths = base_session['ctrl_create_base_paths']
     )
 
-    assert ctrl.code_filename.exists()
-    assert ctrl.code_directory == base_session['code_directory']
-    assert ctrl.code_runtime == base_session['code_runtime']
-    assert ctrl.code_fernet_key == base_session['code_fernet_key']
-    assert ctrl.ctrl_create_base_paths == base_session['ctrl_create_base_paths']
+    [handler.close() for handler in ctrl.log.handlers[:]]
+    
+    assert ctrl.code_filename == Path(base_session['code_filename'])
+    assert ctrl.code_directory == Path(base_session['code_directory'])
+    assert ctrl.tmp_path.exists()
+    assert ctrl.db_path.exists()
+    assert ctrl.log_path.exists()
+
