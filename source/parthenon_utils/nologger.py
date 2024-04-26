cf57bad12c55844013744b8e1017c894ff5a5b8c
Miguel Couy @ Echoenergia <miguel.couy@outlook.com>
2024-04-26 15:19:08 -0300

    Launch of version 1.0.0
    Inserido classes Encrypter, responsável pela criptografia de dados/arquivos, Builder, responsável pelo gerenciamento de diretórios e arquivos, Logger e NoLogger, responsável pela criação de arquivos .log.
    
    Todas as funções são controláveis através da classe Controller.

---
+++ source/parthenon_utils/nologger.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Miguel Freire Couy'
+__credits__ = ['Miguel Freire Couy']
+__version__ = '1.0.0'
+__maintainer__ = 'Miguel Freire Couy'
+__email__ = 'miguel.couy@outlook.com'
+__status__ = 'Production'
+
+class NoLogger:
+    def debug(*args, **kwargs): pass
+    def info(*args, **kwargs): pass
+    def warn(*args, **kwargs): pass
+    def error(*args, **kwargs): pass
+    def critical(*args, **kwargs): pass
