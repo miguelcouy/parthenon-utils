c9a83214809164f38f2eadaa727d59856cdfadbd
Miguel Couy @ Echoenergia <miguel.couy@outlook.com>
2024-04-29 14:38:13 -0300

    Deploy of Version 0.0.4
    Minor changes on Encrypter class.

--- source/build/lib/parthenon_utils/controller.py
+++ source/build/lib/parthenon_utils/controller.py
@@ -2,7 +2,6 @@
 
 __author__ = 'Miguel Freire Couy'
 __credits__ = ['Miguel Freire Couy']
-__version__ = '0.0.1'
 __maintainer__ = 'Miguel Freire Couy'
 __email__ = 'miguel.couy@Ooutlook.com'
 __status__ = 'Production'
